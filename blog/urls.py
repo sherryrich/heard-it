292b4144850acc009f6b62e21c8d5174d60f59cb
sherryrich <ricsherry@gmail.com>
2022-08-01 17:12:37 +0000

    created about.html page

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,10 @@
 from . import views
 from django.urls import path
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
+    path('about', views.generic.TemplateView.as_view(template_name='about.html'), name='about'),
 ]
