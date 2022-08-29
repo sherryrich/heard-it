8e44bab2ef549cef8af7e41237e2aca3fb7ce496
sherryrich <ricsherry@gmail.com>
2022-08-29 16:05:37 +0000

    updated urls.py so that its PEP8 compliant

--- blog/urls.py
+++ blog/urls.py
@@ -7,10 +7,19 @@ urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
     path('contact/', views.contact, name='contact'),
     path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
-    path('about', views.generic.TemplateView.as_view(template_name='about.html'), name='about'),
+    path(
+        'about',
+        views.generic.TemplateView.as_view(template_name='about.html'),
+        name='about'),
     path('search_articles', views.search_articles, name='search_articles'),
     path('new', views.CreatePostView.as_view(), name='new'),
-    path('<slug:slug>/update/', views.UpdatePostView.as_view(), name='post_update'),
-    path('<slug:slug>/delete/', views.DeletePostView.as_view(), name='post_delete'),
+    path(
+        '<slug:slug>/update/',
+        views.UpdatePostView.as_view(),
+        name='post_update'),
+    path(
+        '<slug:slug>/delete/',
+        views.DeletePostView.as_view(),
+        name='post_delete'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
 ]
